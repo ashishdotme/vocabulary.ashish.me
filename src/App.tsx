cf91e2728fed2ff781b9bbdde02e30b2c35360ee
Ashish Patel <ashishsushilpatel@gmail.com>
2019-03-01 02:27:31 +0530

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Flashcard from './components/Flashcard';
+import Home from './components/Home';
+
+import store from './store';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
