fbc6a8565c16b9d061cce6893bd3acf6a5b280fb
Ashish Patel <ashishsushilpatel@gmail.com>
2019-03-06 02:48:23 +0530

    Change design of home and flashcard

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -1,16 +1,78 @@
 import React, { Component } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 interface FlashCardProps {
+  getNextCard: () => void;
+  currentCard: any;
+}
+
+interface FlascardState {
   isFlipped: boolean;
 }
 
-interface FlascardState {}
-
 export default class Flashcard extends Component<FlashCardProps, FlascardState> {
+  myInput: any;
   constructor(props: FlashCardProps) {
     super(props);
+    this.state = { isFlipped: false };
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
+  }
+
+  showBack() {
+    this.setState({ isFlipped: true });
+  }
+
+  showFront() {
+    this.setState({ isFlipped: false });
+  }
+
+  addScore() {
+    this.showFront();
+    this.props.getNextCard();
+  }
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <ReactCardFlip isFlipped={this.state.isFlipped}>
+          <div key="front" onClick={this.handleClick}>
+            <div className="card large">
+              <div className="card-content">
+                <div className="content">{this.props.currentCard['word']}</div>
+              </div>
+            </div>
+          </div>
+
+          <div key="back" onClick={this.handleClick}>
+            <div className="card large">
+              <div className="card-content">
+                <div className="content">{this.props.currentCard['meaning']}</div>
+              </div>
+            </div>
+          </div>
+        </ReactCardFlip>
+        {this.state.isFlipped ? (
+          <div className="bottom-buttons">
+            <button
+              className="btn btn-default incorrect-button"
+              onClick={() => {
+                this.showFront();
+                this.props.getNextCard();
+              }}
+            >
+              I was wrong!
+            </button>
+            <button className="btn btn-default correct-button" onClick={() => this.addScore()}>
+              I was right!
+            </button>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getDeck } from '../actions/cardActions';
 import Flashcard from './Flashcard';
+import './Home.css';
 
 interface HomeProps {
   deck: any;
@@ -18,9 +19,11 @@ class Home extends Component<HomeProps, HomeState> {
     this.state = { currentCardIndex: 0 };
     this.getNextCard = this.getNextCard.bind(this);
   }
+
   componentDidMount() {
     this.props.getDeck();
   }
+
   getNextCard() {
     if (this.props.deck) {
       if (this.state.currentCardIndex === this.props.deck.cards.length - 1) {
@@ -35,7 +38,26 @@ class Home extends Component<HomeProps, HomeState> {
   render() {
     return (
       <div>
-        <Flashcard />
+        <section className="hero is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <div className="section">
+                <div className="row columns">
+                  <div className="column is-one-third" />
+                  <div className="column is-one-third">
+                    {this.props.deck.cards && (
+                      <Flashcard
+                        currentCard={this.props.deck.cards[this.state.currentCardIndex]}
+                        getNextCard={this.getNextCard}
+                      />
+                    )}
+                  </div>
+                  <div className="column is-one-third" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
